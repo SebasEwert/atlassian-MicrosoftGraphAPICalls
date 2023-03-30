56e7224bca299b11768a98ae53c32f76ba7640e4
SebaEwert <sebastian.ewert@otto.de>
2023-03-30 15:16:03 +0200

    Script angepasst für MAC

--- src/GraphAPIBasicCall.java
+++ src/GraphAPIBasicCall.java
@@ -2,17 +2,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class GraphAPIBasicCall {
+    public static void main(String[] args) {
+        String graphEndpoint = "https://graph.microsoft.com/v1.0/me";
 
-    public static void main(String[] args) throws IOException {
-        String script = "$accessToken = 'YOUR_ACCESS_TOKEN';" + "$url = 'https://graph.microsoft.com/v1.0/me/messages';" + "$headers = @{Authorization='Bearer '+$accessToken};" + "$response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get;" + "$response | ConvertTo-Json";
-        String[] command = {"powershell.exe", "-Command", script};
-        Process powerShellProcess = Runtime.getRuntime().exec(command);
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
-        String line;
-        while ((line = stdInput.readLine()) != null) {
-            System.out.println(line);
+        // Replace with your access token
+        String accessToken = "INSERT_ACCESS_TOKEN_HERE";
+
+
+        try {
+            URL url = new URL(graphEndpoint);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // Set the Authorization header with the access token
+            String authHeader = "Bearer " + accessToken;
+            conn.setRequestProperty("Authorization", authHeader);
+
+            // Send the request and read the response
+            int responseCode = conn.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            // Print the response
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
+
+
+  //  This code creates an HTTP GET request to the Microsoft Graph API endpoint https://graph.microsoft.com/v1.0/me
+  //  and sets the Authorization header with the access token. The response is then printed to the console.
+  //  Note that you need to replace INSERT_ACCESS_TOKEN_HERE with your actual access token.
+
+
+
 }
